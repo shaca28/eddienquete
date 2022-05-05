4a9f73f6b45407eb21d81b4592602edd4f414e14
rezox <rezox@DESKTOP-DJU1BUF.localdomain>
2022-05-05 18:52:25 -0300

    Criando segunda pagina html

--- enquetes/views.py
+++ enquetes/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+def enquete(request):
+    return render(request, 'enquete.html')
