e968d77dda06a62b10e7a3e12b0409b88873f1f9
rezox <rezox@DESKTOP-DJU1BUF.localdomain>
2022-05-09 16:11:24 -0300

    Configurando admin

--- enquetes/admin.py
+++ enquetes/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from .models import Pergunta, Resposta
 
-admin.site.register(Pergunta)
-admin.site.register(Resposta)
+class ListandoPergunta(admin.ModelAdmin):
+    list_display = ('id', 'texto', 'data')
+    list_display_links = ('id', 'texto', 'data')
+    search_fields = ('texto',)
+    list_filter = ('texto',)
+    list_per_page = 6
+
+class ListandoResposta(admin.ModelAdmin):
+    list_display = ('id', 'pergunta', 'votos')
+    list_display_links = ('id', 'pergunta', 'votos')
+    search_fields = ('pergunta',)
+    list_filter = ('pergunta',)
+
+
+admin.site.register(Pergunta, ListandoPergunta)
+admin.site.register(Resposta, ListandoResposta)
