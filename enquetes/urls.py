97c74d65d20b3b6b5878200de37d4b30fbcb35bf
rezox <rezox@DESKTOP-DJU1BUF.localdomain>
2022-05-09 14:44:07 -0300

    Alterando view da home

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('enquete/', views.enquete, name='enquete')
+    path('enquete/', views.enquete, name='enquete'),
+    path('<int:pergunta_id>/', views.detalhes, name='detalhes'),
+    path('<int:pergunta_id>/results/', views.resultados, name='resultados'),
+    path('<int:pergunta_id>/vote/', views.votos, name='votos'),
 ]
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 
+from .models import Pergunta
+
 
 def home(request):
-
-    enquetes = {
-        1: 'Pergunta 1',
-        2: 'Pergunta 2',
-        3: 'Pergunta 3',
-    }
-
-    dados = {
-        'nome_das_enquetes': enquetes
-    }
-    return render(request, 'home.html', dados)
+    ultimas_perguntas = Pergunta.objects.order_by('-data')[:5]
+    contexto = {'ultimas_perguntas': ultimas_perguntas}
+    return render(request, 'home.html', contexto)
 
 def enquete(request):
     return render(request, 'enquete.html')
+
+def detalhes(request, pergunta_id):
+    pass
+
+def resultados(request, pergunta_id):
+    pass
+
+def votos(request, pergunta_id):
+    pass              
