431eec62c74b3c19ded3727521870f818525aee7
rezox <rezox@DESKTOP-DJU1BUF.localdomain>
2022-05-09 11:41:33 -0300

    Alterando views e home html com for

--- enquetes/views.py
+++ enquetes/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+
+    enquetes = {
+        1: 'Pergunta 1',
+        2: 'Pergunta 2',
+        3: 'Pergunta 3',
+    }
+
+    dados = {
+        'nome_das_enquetes': enquetes
+    }
+    return render(request, 'home.html', dados)
 
 def enquete(request):
     return render(request, 'enquete.html')
