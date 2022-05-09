b846b2e4e2958e9a9255c17d94fa3acdc014b272
rezox <rezox@DESKTOP-DJU1BUF.localdomain>
2022-05-09 13:42:30 -0300

    Adicionando os modelo de pergunta  e resposta

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+
+class Pergunta(models.Model):
+    texto = models.CharField(max_length=200)
+    data = models.DateTimeField('Data publicação')
+
+    def __str__(self):
+        return self.texto
+
+class Resposta(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    alternativas = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.alternativas
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)        
